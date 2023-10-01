6d228140a80cef16c3b671344caaad20c4d0f815
Vyacheslav <vmk_86@mail.ru>
2023-10-01 11:07:23 +0300

    save3

---
+++ database.py
@@ -0,0 +1,8 @@
+from typing import List
+
+tasks = [
+    {'id': 0, 'title': 'task0', 'description': 'description0', 'status': False},
+    {'id': 1, 'title': 'task1', 'description': 'description1', 'status': False},
+    {'id': 2, 'title': 'task2', 'description': 'description2', 'status': False},
+    {'id': 3, 'title': 'task3', 'description': 'description3', 'status': False},
+]
